ce10418e36153d6b9b77cb334bb60d23ae750a92
raviraj.ghodake <raviraj.ghodake@zemosolabs.com>
2024-12-09 01:55:31 +0530

    candidate service

---
+++ src/services/candidateService.ts
@@ -0,0 +1,87 @@
+import Candidate from "../models/candidateModel";
+import { STATUS_CODES, MESSAGES } from "../utils/constants";
+
+class CandidateService {
+  async createCandidate(data: {
+    name: string;
+    email: string;
+    dob: Date;
+    phone: string;
+    zipcode: string;
+    social_security: string;
+    drivers_license: string;
+  }) {
+    const existingCandidate = await Candidate.findOne({
+      where: { email: data.email },
+    });
+    if (existingCandidate) {
+      throw {
+        status: STATUS_CODES.BAD_REQUEST,
+        message: MESSAGES.ERROR.EMAIL_TAKEN,
+      };
+    }
+
+    const candidate = await Candidate.create(data);
+    return candidate;
+  }
+
+  async getCandidateById(id: number) {
+    try {
+      const candidate = await Candidate.findByPk(id);
+
+      if (!candidate) {
+        throw {
+          status: STATUS_CODES.NOT_FOUND,
+          message: MESSAGES.ERROR.CANDIDATE_NOT_FOUND,
+        };
+      }
+
+      return candidate;
+    } catch (error: any) {
+      throw {
+        status: error.status || STATUS_CODES.INTERNAL_SERVER_ERROR,
+        message: error.message || MESSAGES.ERROR.INTERNAL_SERVER_ERROR,
+      };
+    }
+  }
+
+  async getAllCandidates() {
+    try {
+      const candidates = await Candidate.findAll({
+        attributes: ["name", "adjudication", "status", "location", "date"],
+      });
+      return candidates;
+    } catch (error: any) {
+      throw new Error(MESSAGES.ERROR.FETCH_CANDIDATES_FAILED);
+    }
+  }
+
+  async updateCandidateById(id: number, updatedData: any) {
+    try {
+      const candidate = await Candidate.findByPk(id);
+      if (!candidate) {
+        throw new Error(MESSAGES.ERROR.CANDIDATE_NOT_FOUND);
+      }
+
+      await candidate.update(updatedData);
+      return candidate;
+    } catch (error: any) {
+      throw new Error(error.message || MESSAGES.ERROR.UPDATE_CANDIDATE_FAILED);
+    }
+  }
+
+  async deleteCandidateById(id: number): Promise<void> {
+    try {
+      const candidate = await Candidate.findByPk(id);
+      if (!candidate) {
+        throw new Error(MESSAGES.ERROR.CANDIDATE_NOT_FOUND);
+      }
+
+      await candidate.destroy();
+    } catch (error: any) {
+      throw new Error(error.message || MESSAGES.ERROR.DELETE_CANDIDATE_FAILED);
+    }
+  }
+}
+
+export default new CandidateService();
